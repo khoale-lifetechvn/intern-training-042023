9a984a72caf1ef71c175e3795b44e6e735c77786
DESKTOP-Q1Q61E9\Lifetech <nmvuong263@gmail.com>
2023-04-06 14:59:47 +0700

    Add folder task Docker

---
+++ vuong.nguyen/docker-task/client/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import { Layout,Menu,Skeleton,Table } from "antd";
+
+function App() {
+  const { Header, Content } = Layout;
+  const [isBusy,setBusy] = useState([true])
+  const [data, setData] = useState([])
+  const [columns, setColumns] = useState([])
+  const tables = ["continents","countries","country_languages","languages","regions","country_stats"]
+
+  async function fetchData(table){
+    await axios.get("http://localhost:5000/api/columns",{params: {table: table}}).then( async (res)=>{
+    const data = await res.data
+    setColumns(data)
+    }).catch((err)=>{
+    console.log(err)
+  })
+
+  await axios.get("http://localhost:5000/api",{params: {table: table}}).then( async (res)=>{
+      const data = await res.data
+      setData(data)
+    }).catch((err)=>{
+      console.error(err);
+    })
+  setBusy(false)
+  }
+
+
+  useEffect(()=>{
+    fetchData(tables[1]);
+  },[])
+
+
+  return (
+    <Layout>
+      <Header>
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys = {[tables[1]]}
+          items = {tables.map((e,index) => {
+            return {
+              key: e,
+              label: e.toUpperCase(),
+              onClick: ()=>{fetchData(tables[index]);}
+            };
+          })}
+        />
+      </Header>
+      <Content>
+      {isBusy? <Skeleton active/> : 
+      <Table columns={columns.length>0&&columns.map((item)=>{
+      return {
+        title: item.COLUMN_NAME.toUpperCase(),
+        dataIndex: item.COLUMN_NAME,
+        key: item.COLUMN_NAME,
+      }
+    })} dataSource={data.length>0 && data} rowKey={columns[0].COLUMN_NAME}/>}
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
