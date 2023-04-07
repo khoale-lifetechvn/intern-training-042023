1c020bdc55aa0e65cba67efddbab3435bffb2591
minh.thai <minh.thai.lifetechvn@gmail.com>
2023-04-07 13:10:29 +0700

    update 07/04 13:10

--- minh.thai/client/src/App.js
+++ minh.thai/client/src/App.js
@@ -1,49 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-import  axios  from "axios";
+import axios from "axios";
 import { useEffect, useState } from 'react';
 import { Table, Layout, Menu } from "antd";
-const { Header, Content, Footer } = Layout;
-function App() {
-  const tables = ['continents','countries','country_languages','country_stats','languages','regions']
-  const [data,setData] = useState([])
-  const [columns,setColumns] = useState([])
 
+const { Header, Content, Footer } = Layout;
+
+function App() {
+  // Khai báo các bảng dữ liệu cần hiển thị
+  const tables = ['continents','countries','country_languages','country_stats','languages','regions']
+
+  // Sử dụng useState hooks để lưu trữ dữ liệu và cột cho bảng dữ liệu hiển thị
+  const [data, setData] = useState([])
+  const [columns, setColumns] = useState([])
+
+  // Hàm lấy dữ liệu từ API
   async function getData(table) {
-    const url = "http://localhost:3000/"+`${table}`
-    console.log(url);
-    await axios.get(url+"/columns").then(async res=> {
+    // Xây dựng URL cho API
+    const url = "http://localhost:3001/" + `${table}`
+
+    // Lấy thông tin cột của bảng dữ liệu
+    await axios.get(url + "/columns").then(async res => {
       const data = await res.data
       setColumns(data)
     })
 
-    await axios.get(url).then(async res=> {
+    // Lấy dữ liệu từ bảng dữ liệu
+    await axios.get(url).then(async res => {
       const data = await res.data
       setData(data)
     });
   }
 
-
-  useEffect(()=>{
+  // Sử dụng useEffect hook để lấy dữ liệu cho bảng dữ liệu đầu tiên khi ứng dụng được khởi chạy
+  useEffect(() => {
     getData(tables[0]);
-  },[])
+  }, [])
+
+  // Sử dụng Ant Design Layout và Menu để tạo giao diện cho ứng dụng
   return (
     <Layout>
       <Header>
         <Menu
-        theme='dark'
-        mode="horizontal"
-        items={tables.map((item,index)=>{
-          return{
-            key: item,
-            label: item,
-            onClick:()=>getData(tables[index])
-          }
-        })}
-          />
+          theme='dark'
+          mode="horizontal"
+          // Tạo các mục trong menu tương ứng với các bảng dữ liệu
+          items={tables.map((item, index) => {
+            return {
+              key: item,
+              label: item,
+              onClick: () => getData(tables[index])
+            }
+          })}
+        />
       </Header>
       <Content>
-      <Table dataSource={data} columns={columns.map(item=>{return{key: item.name, title: item.name, dataIndex: item.name}})}/>
+        {/* Sử dụng Ant Design Table để hiển thị dữ liệu trong bảng */}
+        <Table dataSource={data} columns={columns.map(item => {
+          return { key: item.name, title: item.name, dataIndex: item.name }
+        })} />
       </Content>
     </Layout>
   );
--- minh.thai/server/index.js
+++ minh.thai/server/index.js
@@ -5,53 +5,13 @@ const cors = require('cors')
 const app = express();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysql',
   user: 'Minh',
-  password: 'Tuongminh2001',
+  password: 'Tuongminh2001@',
   database: 'my_database'
 });
 app.use(cors())
-// app.get('/continents', (req, res) => {
-//   connection.query('SELECT * FROM continents', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-// });
 
-// app.get('/countries', (req, res) => {
-//   connection.query('SELECT * FROM countries', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-// });
-
-// app.get('/country_languages', (req, res) => {
-//   connection.query('SELECT * FROM country_languages', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-// });
-
-// app.get('/country_stats', (req, res) => {
-//   connection.query('SELECT * FROM country_stats', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-// });
-
-// app.get('/languages', (req, res) => {
-//   connection.query('SELECT * FROM languages', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-// });
-
-// app.get('/regions', (req, res) => {
-//   connection.query('SELECT * FROM regions', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-// });
 app.get('/:table/columns',(req, res)=>{
   const table = req.params.table
   const query = "select COLUMN_NAME as name from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME= '" + `${table}` +"'"
@@ -74,6 +34,6 @@ app.get('/', (req, res) => {
   res.send('Hello Minh');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
 });
