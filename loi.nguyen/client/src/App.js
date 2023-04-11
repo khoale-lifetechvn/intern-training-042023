29fc0525339c800256f020780181f3c701e5e6d7
Hong Loi <130038058+loinguyen-lifetechvn@users.noreply.github.com>
2023-04-11 09:06:37 +0700

    update data

--- loi.nguyen/client/src/App.js
+++ loi.nguyen/client/src/App.js
@@ -8,7 +8,37 @@ function App() {
   const [isBusy,setBusy] = useState([true])
   const [data, setData] = useState([])
   const [columns, setColumns] = useState([])
+  
   const tables = ["continents", "countries", "country_languages", "country_stats","languages","regions"];
+
+  const [table, setTable] = useState("");
+  const [values, setValues] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      table: table,
+      values: values.map((value) => `'${value}'`).join(","),
+    };
+    try {
+      const response = await axios.post("http://localhost:5000/api/insertData", data);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleValueChange = (index, value) => {
+    const newValues = [...values];
+    newValues[index] = value;
+    setValues(newValues);
+  };
+
+
+
+
+
+
 
   async function fetchData(table){
     await axios.get("http://localhost:5000/api/columns",{params: {table: table}}).then( async (res)=>{
@@ -26,6 +56,8 @@ function App() {
     })
   setBusy(false)
   }
+
+
 
 
   useEffect(()=>{
@@ -49,6 +81,27 @@ function App() {
         />
     
       <Content>
+      {/* Form to insert */}
+      <form onSubmit={handleSubmit}>
+      <label>
+        Table:
+        <input type="text" value={table} onChange={(e) => setTable(e.target.value)} />
+      </label>
+      <br />
+      {/* {columns.map((column, index) => (
+        <label key={index}>
+          {column}:
+          <input
+            type="text"
+            value={values[index] || ""}
+            onChange={(e) => handleValueChange(index, e.target.value)}
+          />
+        </label>
+      ))} */}
+      <br />
+      <button type="submit">Insert Data</button>
+    </form>
+     {/* Table show data */}
       {isBusy? <Skeleton active/> : 
     <table>
     <thead>
--- loi.nguyen/server/app.js
+++ loi.nguyen/server/app.js
@@ -6,32 +6,44 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const connection = mysql.createPool({
-  host:'mysql',
-  user:'root',
-  password:'root',
-  database:'countries'
+  host: 'mysql',
+  user: 'root',
+  password: 'root',
+  database: 'countries'
 })
+
 app.use(cors())
 
-connection.query("SELECT 1",(err, result)=>{
-  err? console.error(err):console.log("Connect to database successfullly");
+connection.query("SELECT 1", (err, result) => {
+  err ? console.error(err) : console.log("Connect to database successfullly");
 })
 
 app.get('/api', (req, res) => {
-    var sql = "SELECT * FROM "+`${req.query.table}`;
-    
-    connection.query(sql, function(err, results) {
-      if (err)  throw err;
-      res.send(results);
-    });
+  var sql = "SELECT * FROM " + `${req.query.table}`;
+
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
+    res.send(results);
   });
-  app.get('/api/columns', (req, res) => {
-    var sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '"+`${req.query.table}`+"' ORDER BY ORDINAL_POSITION";
-    connection.query(sql, function(err, results) {
-      if (err)  throw err;
-      res.send(results);
-    });
+});
+app.get('/api/columns', (req, res) => {
+  var sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + `${req.query.table}` + "' ORDER BY ORDINAL_POSITION";
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
+    res.send(results);
   });
+});
+
+app.post('/api/insert', (req, res) => {
+  const table = req.body.table;
+  const values = req.body.values;
+  const sql = `INSERT INTO ${table} VALUES (${values})`;
+  connection.query(sql, function(err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
