160cdbad51f9633a74010bcdeb2ac7e7727407c1
Hong Loi <130038058+loinguyen-lifetechvn@users.noreply.github.com>
2023-04-26 16:52:41 +0700

    can show grid

--- loi.nguyen/task_firebase/lib/ui/base_widget/find_users_list.dart
+++ loi.nguyen/task_firebase/lib/ui/base_widget/find_users_list.dart
@@ -7,6 +7,7 @@ import 'package:task_firebase/ui/resources/assets_manager.dart';
 import 'package:task_firebase/ui/resources/color_manager.dart';
 import 'package:task_firebase/ui/resources/routes_manager.dart';
 import 'package:task_firebase/ui/resources/styles_manager.dart';
+import 'package:task_firebase/ui/resources/values_manager.dart';
 
 class FindUserList extends StatefulWidget {
   const FindUserList(
@@ -21,6 +22,14 @@ class FindUserList extends StatefulWidget {
 class _FindUserListState extends State<FindUserList> {
   late List<UserModel> allUser;
   List<UserModel> currentUsers = [];
+
+  bool isShowList = true;
+
+  void updateState() {
+    setState(() {
+      isShowList = !isShowList;
+    });
+  }
 
   @override
   void initState() {
@@ -60,29 +69,54 @@ class _FindUserListState extends State<FindUserList> {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           SearchItem(
               hint: 'Search user',
               onChanged: (v) {
                 findUsers(v);
               }),
-          const SizedBox(height: 24),
+          TextButton(
+              onPressed: updateState,
+              child: isShowList
+                  ? Text(
+                      'Show Grid',
+                      style: getTitleText(color: ColorManager.black),
+                    )
+                  : Text(
+                      'Show Grid',
+                      style: getTitleText(color: ColorManager.blue),
+                    )),
           Expanded(
-            child: ListView.separated(
-              itemCount: currentUsers.length,
-              separatorBuilder: (_, __) => Divider(
-                  height: 16, color: ColorManager.greyBG, thickness: 0.4),
-              itemBuilder: (_, index) => itemUser(
-                model: currentUsers[index],
-              ),
-            ),
+            child: isShowList
+                ? ListView.separated(
+                    itemCount: currentUsers.length,
+                    separatorBuilder: (_, __) => Divider(
+                        height: 16, color: ColorManager.greyBG, thickness: 0.4),
+                    itemBuilder: (_, index) => itemUserList(
+                      model: currentUsers[index],
+                    ),
+                  )
+                : GridView.builder(
+                    itemCount: currentUsers.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.8,
+                            crossAxisSpacing: 16,
+                            mainAxisSpacing: 16),
+                    padding: const EdgeInsets.all(16),
+                    itemBuilder: (_, index) => itemUserGrid(
+                      model: currentUsers[index],
+                    ),
+                  ),
           ),
         ],
       ),
     );
   }
 
-  Widget itemUser({required UserModel model}) {
+  Widget itemUserList({required UserModel model}) {
     return ListTile(
       leading: model.img.isEmpty
           ? AspectRatio(aspectRatio: 1, child: Image.asset(ImageAssets.mewo))
@@ -99,6 +133,35 @@ class _FindUserListState extends State<FindUserList> {
       onTap: () {
         locator<GetNavigation>().to(RouterPath.detailAuthor, arguments: model);
       },
+    );
+  }
+
+  Widget itemUserGrid({required UserModel model}) {
+    return InkWell(
+      onTap: () {
+        locator<GetNavigation>().to(RouterPath.detailAuthor, arguments: model);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: ColorManager.blue,
+            border: Border.all(color: ColorManager.greyForm, width: 0.3)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              model.showName,
+              style: getTitleText(color: ColorManager.white),
+            ),
+            const SizedBox(height: AppSize.s12),
+            Image.network(
+              model.showImg,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
